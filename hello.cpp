ab3fc23ee9c3b3d76847af4a5edfba22dc6d434b
Chandna-Soni <97679691+Chandna-Soni@users.noreply.github.com>
2022-10-08 21:58:00 +0530

    Add files via upload

---
+++ hello.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    
+   int n;
+   cin>>n;
+
+if (n%2==0)
+{
+    printf("this is even no:");
+    
+}
+else{
+printf("odd no");
+}
+
+return 0;
+    
+    }
